c0ee8fc3d5cc261378d5547493d46ca5f81872a0
sooryanarayanahebbar <109686124+sooryanarayanahebbar@users.noreply.github.com>
2024-03-27 16:14:37 +0530

    Add files via upload

---
+++ src/main/java/com/tools/rental/ToolsRentalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tools.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsRentalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsRentalDemoApplication.class, args);
+	}
+
+}
